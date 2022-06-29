3ae6494607cb77e60eaff00889471b31a34ab278
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-06-29 16:23:16 +0200

    {Laravel} Compleated: Crude Show Page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,18 +4,20 @@
   <div class="container my-4">
     <h1>Comic Details </h1>
 
-    <div class="row">
+    <div class="row my-4">
 
-      <div class="col-6">
+      <div class="col-6 d-flex flex-column justify-content-center align-items-center">
           <h3>{{ $comic->title }}</h3>
-          <span>{{ $comic->type }}</span>
+          <span class="bg-warning my-2 p-1 rounded">{{ $comic->type }}</span>
       </div>
 
-      <div class="col-6">
+      <div class="col-6 d-flex justify-content-center align-items-center">
         <img src="{{ $comic->image }}" alt="{{ $comic->title }}">
       </div>
 
     </div>
+
+    <a class="btn btn-success" href="{{ route('comics.index') }}">BACK</a>
     
   </div>
 
